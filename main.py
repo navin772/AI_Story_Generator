395bfc4c67c75d95c78d549ea7f32a15b329086c
Navin Chandra <navinchandra772@gmail.com>
2023-09-10 14:15:55 +0530

    Use st.cache for audio generation

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ from gtts import gTTS
 
 from workflow_text_to_text import generate_story_from_text
 
+@st.cache_data(persist=True)
+def generate_audio_from_story(text):
+    speech = gTTS(text=text, lang='en', slow=False)
+    speech.save("story.mp3")
+    return "story.mp3"
+
 st.set_page_config(
-    page_title="Retail product detection", layout="wide"
+    page_title="AI Story Generator", layout="wide", page_icon="📖"
 )
 
 
-st.sidebar.write("Select the genre/theme of the story:")
+st.sidebar.markdown("### Select the genre/theme of the story:")
 
-story_theme = st.sidebar.radio("Genre", ("Horror", "Action", "Romance", "Comedy", "Historical", "Science Fiction"))
+story_theme = st.sidebar.radio("Genre", ("Horror :ghost:", "Action :man-running:", "Romance :heart:", "Comedy :laughing:", "Historical :hourglass_flowing_sand:", "Science Fiction :rocket:"))
+
+selected_theme = story_theme.split(":")[0].strip()
 
 theme_based_prompts = {
     "Horror": "Write a horror story that ends mysteriously using: ",
@@ -22,19 +30,22 @@ theme_based_prompts = {
     "Science Fiction": "Write a science fiction story using: "
 }
 
-st.markdown("# AI Story Generator")
+st.markdown("# AI Story Generator :book:")
 
-st.markdown("## Choose the input type you want to give to start with the story generation")
+with st.expander("About this app :bulb:", expanded=False):
+    st.markdown("This app uses the **Clarifai AI** engine to generate stories based on the input you provide using **LLM** models. You can either upload an image or enter some text to get started. The app will then generate a story based on the input you provide. The story will be generated using the theme you choose, you can select the theme from the sidebar. The app will also generate an **audio file** of the story for you to listen to. You can **download the story as a text file or an audio file (mp3)**.")
 
-input_type = st.radio("Input type", ("Text", "Image"))
+st.markdown("## Choose the input type for generating the story")
 
-if input_type == "Text":
+input_type = st.radio("Input type", ("Text :pencil:", "Image"))
 
-    st.markdown("### Enter the sentences you want to have your story revolve around")
+if input_type == "Text :pencil:":
+
+    st.markdown("### Enter the sentences you want to have your story revolve around: ")
 
     input_text = st.text_area("Enter the text here", height=100)
 
-    theme_based_input = theme_based_prompts[story_theme] + " " + input_text
+    theme_based_input = theme_based_prompts[selected_theme] + " " + input_text
 
     if st.button("Generate story"):
         with st.status("Generating story...", expanded=True) as status_text:
@@ -52,10 +63,18 @@ if input_type == "Text":
         story_lines = story.split('\n')
         formatted_story = "\n".join(["##### " + line for line in story_lines])
         
-        st.markdown(formatted_story)
+        with st.expander("View story", expanded=True):
+            st.markdown(formatted_story)
         
-        speech = gTTS(text=story, lang='en', slow=False)
-        speech.save("story.mp3")
+        with st.status("Generating audio...", expanded=True) as status_audio:
+
+            st.write("Generating your audio file...")
+            st.write("This may take about 10-20 seconds")
+
+            generate_audio_from_story(story)
+            
+            status_audio.update(label="Audio generated...")
+        
         st.audio("story.mp3")
         st.download_button('Download story as mp3 file', 'story.mp3', 'story.mp3')
 
